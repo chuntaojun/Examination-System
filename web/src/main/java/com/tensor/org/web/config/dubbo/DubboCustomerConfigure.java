72f4be2ae9426bd139214a2bfcd40bf6adab2b66
chuntaojun <liaochuntao@live.com>
2019-05-13 14:39:32 +0800

    feat:

---
+++ web/src/main/java/com/tensor/org/web/config/dubbo/DubboCustomerConfigure.java
@@ -0,0 +1,21 @@
+//package com.tensor.org.web.config.dubbo;
+//
+//import org.apache.dubbo.config.ConsumerConfig;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * @author liaochuntao
+// */
+//@Configuration
+//public class DubboCustomerConfigure {
+//
+//    @Bean
+//    public ConsumerConfig consumerConfig() {
+//        ConsumerConfig consumerConfig = new ConsumerConfig();
+//        consumerConfig.setCheck(false);
+//        consumerConfig.setTimeout(40000);
+//        return consumerConfig;
+//    }
+//
+//}
